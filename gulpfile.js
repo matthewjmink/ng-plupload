feda0d0f27d91bd469afe8640fcbe68b68d0f0cd
Matt Mink <matthewjmink@gmail.com>
2016-08-19 12:33:53 -0500

    * Add gulp-strip-debug and gulp-strip-comments to remove comments dist files

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,19 @@
 'use strict';
 
 var gulp = require('gulp');
-var concat = require('gulp-concat');  
-var rename = require('gulp-rename');  
+var concat = require('gulp-concat');
+var rename = require('gulp-rename');
 var uglify = require('gulp-uglify');
+var stripComments = require('gulp-strip-comments');
+var stripDebug = require('gulp-strip-debug');
 var ngFilesort = require('gulp-angular-filesort');
 
 gulp.task('default', function(callback) {
   return gulp.src(['./src/**/*' ])
     .pipe(ngFilesort())
     .pipe(concat('ng-plupload.js'))
+    .pipe(stripDebug())
+    .pipe(stripComments())
     .pipe(gulp.dest('./dist'))
     .pipe(rename('ng-plupload.min.js'))
     .pipe(uglify())
