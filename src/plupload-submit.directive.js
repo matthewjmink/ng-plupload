f4485d0c67f573db58c271cb9da5849d850ba86b
Matt Mink <matthewjmink@gmail.com>
2016-08-19 07:39:34 -0500

    Converting indent to 4 spaces

--- src/plupload-submit.directive.js
+++ src/plupload-submit.directive.js
@@ -1,25 +1,25 @@
 (function() {
-  'use strict'
+    'use strict'
 
-  angular.module('plupload')
+    angular.module('plupload')
     .directive('pluploadSubmit', pluploadSubmit);
 
-  pluploadSubmit.$inject = ['pluploadService'];
-  function pluploadSubmit (pluploadService) {
-    return {
-      scope: {
-        id: '=pluploadId'
-      },
-      link: pluploadLinkFunc
-    };
+    pluploadSubmit.$inject = ['pluploadService'];
+    function pluploadSubmit (pluploadService) {
+        return {
+            scope: {
+                id: '=pluploadId'
+            },
+            link: pluploadLinkFunc
+        };
 
-    function pluploadLinkFunc(scope, element, attrs) {
-      element[0].onclick = function(){
-        var uploader = pluploadService.get(scope.id);
-        if(uploader){
-          uploader.start();
+        function pluploadLinkFunc(scope, element, attrs) {
+            element[0].onclick = function(){
+                var uploader = pluploadService.get(scope.id);
+                if(uploader){
+                    uploader.start();
+                }
+            };
         }
-      };
     }
-  }
 })();
